5454bfa27c5f990c771aff1b8cdcef9122cc2fb0
Joao Andrade Junior <junior.bsdlages@hotmail.com>
2020-02-12 08:08:59 -0400

    Versao muito ruim

---
+++ src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date hoje = new Date();
+		
+		System.out.print("Numero do Quarto: ");
+		int quarto = sc.nextInt();
+		System.out.print("Data de Check-in (dd/mm/aaaa): ");
+		Date checkin = sdf.parse(sc.next());
+		System.out.print("Data de Check-out (dd/mm/aaaa): ");
+		Date checkout = sdf.parse(sc.next());
+		
+		if (checkin.before(hoje) || (checkout.before(hoje))) {
+			System.out.println("Erro na reserva! A data do Ckeck-out não pode ser anterior a data atual.");
+		}
+		
+		if (!checkout.after(checkin)) {
+			System.out.println("Erro na reserva! A data do Ckeck-out não pode ser anterior ao Check-in");
+		}
+		else {
+			Reservation reserva = new Reservation(quarto, checkin, checkout);
+			System.out.println("Reserva: " + reserva);
+			System.out.println();
+		}
+		
+		System.out.println("Digite as novas datas da reserva: ");
+		System.out.print("Data de Check-in (dd/mm/aaaa): ");
+		checkin = sdf.parse(sc.next());
+		System.out.print("Data de Check-out (dd/mm/aaaa): ");
+		checkout = sdf.parse(sc.next());
+		
+		if (checkin.before(hoje) || (checkout.before(hoje))) {
+			System.out.println("Erro na reserva! A data do Ckeck-out não pode ser anterior a data atual.");
+		}
+		else {
+			Reservation reserva = new Reservation(quarto, checkin, checkout);
+			System.out.println("Reserva: " + reserva);
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+
+}
---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,74 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation() {
+	}
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+	
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	
+	public Date getCheckIn() {
+		return checkIn;
+	}
+	
+	public Date getCheckOut() {
+		return checkOut;
+	}
+	
+	/* public void setCheckIn(Date checkIn) {
+		this.checkIn = checkIn;
+	}
+	CheckIn não deve ser alterado livremente, por isso não pode ter setCheckIn
+	 
+	public void setCheckOut(Date checkOut) {
+		this.checkOut = checkOut;
+	}
+	Mesmo caso do setCheckIn
+	*/
+	
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime(); // long pq o numero é um milisegundos. getTime é função de tempo em Java
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // Converte milisegundos em dias pelo TimeUnit
+	}
+	
+	public void updateDates(Date checkin, Date checkout) {
+		this.checkIn = checkin;
+		this.checkOut = checkout;
+	}
+	
+	@Override
+	public String toString() {
+		return "Quarto "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " noites.";
+	}
+	
+}
